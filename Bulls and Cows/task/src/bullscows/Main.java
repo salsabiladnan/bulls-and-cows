47709a2e9aa6113894718944da1995a5a7e2c314
salsabila.adnan <salsabilaadnan11@gmail.com>
2020-10-28 23:42:18 +0700

    Stage 3/7: Secret code

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -3,36 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String code = "1101";
-
         Scanner scanner = new Scanner(System.in);
+        
+        long pseudoRandomNumber = System.nanoTime();
+        
+        String random = String.valueOf(pseudoRandomNumber);
+        
+        int input = scanner.nextInt(); 
+        
+        StringBuilder res = new StringBuilder();
 
-        String input = scanner.nextLine(); 
-
-        int bulls = 0;
-        int cows = 0;
-
-        for (int i = 0; i < 4; i++) {
-            String cek = String.valueOf(input.charAt(i));
-            if (code.contains(cek)) {
-                String cekCode = String.valueOf(code.charAt(i));
-                if (cek.equals(cekCode)) {
-                    bulls += 1;
+        if (input > 10) {
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", input);
+        } else {
+            for (int i = 0; i < input; i++) {
+                String resStr = res.toString();
+                String randomI = String.valueOf(random.charAt(i));
+                if (!resStr.contains(randomI)) {
+                    res.append(randomI);
                 } else {
-                    cows += 1;
+                    input += 1;
                 }
             }
-        }
-
-        if (bulls == 0 && cows == 0) {
-            System.out.printf("Grade: None. The secret code is %s.", code);
-        } else if (bulls > 0 && cows == 0) {
-            System.out.printf("Grade: %d bull(s). The secret code is %s.", bulls, code);
-        } else if (bulls == 0 && cows > 0) {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.", cows, code);
-        } else {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is $s.", bulls, cows, code);
+            System.out.printf("The random secret number is %s.", res);
         }
     }
 }
